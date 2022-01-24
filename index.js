4ae824af00f32f628cd46976ce8c65db390005ff
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-01-24 12:20:55 -0300

    Add .post response to the route '/sign-up'

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+
+server.use(cors());
+server.use(json());
+
+const users = [];
+
+server.post('/sign-up', (req, res) => {
+
+    const user = req.body;
+
+    users.push(user);
+    res.send("OK");
+
+})
+
 
 server.listen(5000);
