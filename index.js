302f9594e300a443704de38e036346746c43dfdc
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-01-24 12:37:50 -0300

    Add .post response to the route '/tweets'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 
 const server = express();
@@ -7,6 +7,9 @@ server.use(cors());
 server.use(json());
 
 const users = [];
+const tweets = [];
+let post = {};
+let tweetsFull = [];
 
 server.post('/sign-up', (req, res) => {
 
@@ -15,7 +18,29 @@ server.post('/sign-up', (req, res) => {
     users.push(user);
     res.send("OK");
 
-})
+});
 
+server.post('/tweets', (req, res) => {
+
+    const tweet = req.body;
+
+    tweets.push(tweet);
+    res.send("OK");
+
+    post = {};
+    tweetsFull = [];
+    for (let i = 0; i < tweets.length; i++) {
+        for (let j = 0; j < users.length; j++) {
+            if (tweets[i].username === users[j].username) {
+                post = {
+                    username: tweets[i].username,
+                    avatar: users[j].avatar,
+                    tweet: tweets[i].tweet
+                }
+            }
+            tweetsFull.push(post);
+        }
+    }
+});
 
 server.listen(5000);
